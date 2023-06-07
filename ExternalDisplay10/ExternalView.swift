732c45486d4a1db4738600ad40de029813001208
hayate kakizaki <hayate.k.0704@gmail.com>
2023-06-07 16:18:00 +0900

    画面の左上にRectangle表示

---
+++ ExternalDisplay10/ExternalView.swift
@@ -0,0 +1,36 @@
+//
+//  ExternalView.swift
+//  ExternalDisplay10
+//
+//  Created by cmStudent on 2023/05/24.
+//
+
+//if externalDisplayContent.isShowingOnExternalDisplay {
+//    Text("接続済み")
+//} else {
+//    Text("切断している")
+//}
+
+import SwiftUI
+
+// 外部ディスプレイに表示するためのview
+struct ExternalView: View {
+    // MARK: - Property Wrappers
+    @EnvironmentObject var externalDisplayContent: ExternalDisplayContent
+    // MARK: - body
+    var body: some View {
+        GeometryReader { proxy in
+            Rectangle()
+                .frame(width: 100, height: 100)
+                .cornerRadius(50)
+                .ignoresSafeArea()
+        }
+    } // body
+} // view
+
+// MARK: - Preview
+struct ExternalView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExternalView()
+    }
+}
