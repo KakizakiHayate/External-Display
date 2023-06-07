19fa60f07a80b12cd20946d72ab6017b5c90b747
hayate kakizaki <hayate.k.0704@gmail.com>
2023-06-07 16:17:10 +0900

    ViewModelのファイルを分けた

---
+++ ExternalDisplay10/ExternalDisplayContent.swift
@@ -0,0 +1,14 @@
+//
+//  ExternalDisplayContent.swift
+//  ExternalDisplay10
+//
+//  Created by cmStudent on 2023/06/07.
+//
+
+import Foundation
+
+// 外部ディスプレイのviewと端末のviewで情報を繋ぐためのObservavleObject
+class ExternalDisplayContent: ObservableObject {
+    // 外部ディスプレイを表示しているか判定
+    @Published var isShowingOnExternalDisplay = false
+}
