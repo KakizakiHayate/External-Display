e3ce6bcb70fcf9f47fda96a5c665d373d0b86036
kakizaki hayate <hayate.h.0704@gmail.com>
2023-06-13 11:15:42 +0900

    Colorをextensionした

---
+++ ExternalDisplay10/Extension/ColorExtension.swift
@@ -0,0 +1,14 @@
+//
+//  ColorExtension.swift
+//  ExternalDisplay10
+//
+//  Created by 柿崎逸 on 2023/06/11.
+//
+
+import SwiftUI
+
+extension Color {
+    static let customColorGray = Color(red: 76 / 255, green: 76 / 255, blue: 109 / 255)
+    static let customColorGreen = Color(red: 27 / 255, green: 156 / 255, blue: 133 / 255)
+    static let customColorYellow = Color(red: 255 / 255, green: 255 / 255, blue: 146 / 255)
+}
