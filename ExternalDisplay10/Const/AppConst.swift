a87eb95d1ef68187a2d1da31bafe25e0d9bd168d
kakizaki hayate <hayate.h.0704@gmail.com>
2023-06-14 22:09:33 +0900

    AppConst追加

--- ExternalDisplay10/Const/AppConst.swift
+++ ExternalDisplay10/Const/AppConst.swift
@@ -15,6 +15,16 @@ enum AppConst {
         static let resetTapCount = 0
     }
     enum Text {
+        /// 図形を選択してください
         static let selectShapes = "図形を選択してください"
+        static let confirmation = "確認"
+        static let cancel = "キャンセル"
+        static let attention = "注意"
+        static let ok = "ok"
+        static let unchangeableShape = "図形を変更できません"
+        static let selectShapeQuestion = "この図形を選択しますか？\n変更できなくなります"
+        /// 選択する
+        static let selected = "選択する"
+        static let buttonTap = "ボタンをタップ！"
     }
 }
