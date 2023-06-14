e000f964a6dc8d11375af3514ca68321b00a5b24
kakizaki hayate <hayate.h.0704@gmail.com>
2023-06-14 22:32:01 +0900

    ファイル移動

---
+++ ExternalDisplay10/Screen/Display/ExternalDisplayContent.swift
@@ -0,0 +1,42 @@
+//
+//  ExternalDisplayContent.swift
+//  ExternalDisplay10
+//
+//  Created by cmStudent on 2023/06/07.
+//
+
+import Foundation
+
+// 外部ディスプレイのviewと端末のviewで情報を繋ぐためのObservavleObject
+class ExternalDisplayContent: ObservableObject {
+    // 外部ディスプレイを表示しているか判定
+    @Published var isShowingOnExternalDisplay = false
+    @Published var shapes: [String?] = []
+    @Published var shapeTotalWidth = CGFloat(0)
+    @Published var shapeTotalHeight = CGFloat(0)
+    @Published var x = CGFloat(0)
+    @Published var y = CGFloat(0)
+    @Published var screenWidth = 0.0
+    @Published var screenHeight = 0.0
+    @Published var tapCount = 0
+}
+
+extension ExternalDisplayContent {
+    // MARK: - Methods
+    ///折り返すときに呼ばれる
+    func offsetShapes() {
+        if tapCount != 0 && screenWidth < shapeTotalWidth {
+            shapeTotalWidth = AppConst.NumericalValue.width
+            shapeTotalHeight += AppConst.NumericalValue.height
+            if shapeTotalHeight > screenHeight {
+                x = AppConst.NumericalValue.resetValue
+                y = AppConst.NumericalValue.resetValue
+                shapeTotalHeight = AppConst.NumericalValue.resetValue
+                tapCount = AppConst.NumericalValue.resetTapCount
+            } else {
+                x = AppConst.NumericalValue.resetValue
+                y += AppConst.NumericalValue.height
+            }
+        }
+    }
+}
---
+++ ExternalDisplay10/Screen/Display/ExternalView.swift
@@ -0,0 +1,56 @@
+//
+//  ExternalView.swift
+//  ExternalDisplay10
+//
+//  Created by cmStudent on 2023/05/24.
+//
+
+//if externalDisplayContent.isShowingOnExternalDisplay {
+//    Text("接続済み")
+//} else {
+//    Text("切断している")
+//}
+
+import SwiftUI
+
+// 外部ディスプレイに表示するためのview
+struct ExternalView: View {
+    // MARK: - Property Wrappers
+    @EnvironmentObject var externalDisplayContent: ExternalDisplayContent
+    // MARK: - body
+    var body: some View {
+        ZStack {
+            Color.customColorGray
+                .ignoresSafeArea(.all)
+            GeometryReader { proxy in
+                ForEach(externalDisplayContent.shapes, id: \.self) { shape in
+                    if proxy.size.width > externalDisplayContent.shapeTotalWidth {
+                        if shape == "circle" {
+                            Circle()
+                                .frame(width: AppConst.NumericalValue.width, height: AppConst.NumericalValue.height)
+                                .foregroundColor(Color.customColorGreen)
+                                .offset(x: externalDisplayContent.x, y: externalDisplayContent.y)
+                        } else if shape == "rectangle" {
+                            Rectangle()
+                                .frame(width: AppConst.NumericalValue.width, height: AppConst.NumericalValue.height)
+                                .cornerRadius(10)
+                                .foregroundColor(Color.customColorGreen)
+                                .offset(x: externalDisplayContent.x, y: externalDisplayContent.y)
+                        }
+                    }
+                }
+                .onAppear {
+                    externalDisplayContent.screenWidth = proxy.size.width
+                    externalDisplayContent.screenHeight = proxy.size.height
+                }
+            }
+        }
+    } // body
+} // view
+
+// MARK: - Preview
+struct ExternalView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExternalView()
+    }
+}
